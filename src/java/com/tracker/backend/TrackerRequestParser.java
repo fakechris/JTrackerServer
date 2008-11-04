0bcf25c39110f95cecf860008d5fa846748d9fdf
bo <bo@75a8191c-d363-11dd-86c2-0f8696b7b6f9>
2008-11-04 21:43:25 +0000

    more huge updates

--- src/java/com/tracker/backend/TrackerRequestParser.java
+++ src/java/com/tracker/backend/TrackerRequestParser.java
@@ -14,13 +14,16 @@ import java.util.TreeMap;
 import java.net.URLDecoder;
 import java.util.BitSet;
 import java.util.Calendar;
+import java.util.List;
 import java.util.Random;
 import java.util.Vector;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.NoResultException;
 import javax.persistence.Persistence;
+import javax.persistence.Query;
 
 /**
  *
@@ -60,7 +63,7 @@ public class TrackerRequestParser {
         return(remoteAddress);
     }
 
-    public TreeMap parseRequest()
+    public TreeMap parseRequest() throws Exception
     {
         if(requestParams == null || remoteAddress == null) {
             return(null);
@@ -69,9 +72,10 @@ public class TrackerRequestParser {
         EntityManager em = emf.createEntityManager();
         TreeMap responseParams = new TreeMap();
         String infoHash, peerId;
-        Long uploaded, downloaded, left;
+        String event = new String();
+        Long uploaded, downloaded, left, port;
+        boolean returnSeeds = true;
         Integer numPeersToReturn = new Integer(50);
-        Integer port;
         Torrent t;
         Peer p;
 
@@ -95,39 +99,39 @@ public class TrackerRequestParser {
         }
 
         // check for peer id
-        if(!requestParams.containsKey((String)"peer id")) {
-            return(parseFailed("missing peer id!"));
+        if(!requestParams.containsKey((String)"peer_id")) {
+            return(parseFailed("missing peer_id!"));
         }
 
-        peerId = (String)requestParams.get((String)"peer id");
+        peerId = (String)requestParams.get((String)"peer_id");
 
         // check for port-number
         if(!requestParams.containsKey((String)"port")) {
             return(parseFailed("missing port number!"));
         }
 
-        port = Integer.getInteger((String)requestParams.get((String)"port"));
+        port = Long.parseLong((String)requestParams.get((String)"port"));
 
         // check for uploaded
         if(!requestParams.containsKey((String)"uploaded")) {
             return(parseFailed("missing 'uploaded' field!"));
         }
 
-        uploaded = Long.getLong((String)requestParams.get((String)"uploaded"));
+        uploaded = Long.parseLong((String)requestParams.get((String)"uploaded"));
 
         // check for downloaded
         if(!requestParams.containsKey((String)"downloaded")) {
             return(parseFailed("missing 'downloaded' field!"));
         }
 
-        downloaded = Long.getLong((String)requestParams.get((String)"downloaded"));
+        downloaded = Long.parseLong((String)requestParams.get((String)"downloaded"));
 
         // check for left
         if(!requestParams.containsKey((String)"left")) {
             return(parseFailed("missing 'left' field!"));
         }
 
-        left = Long.getLong((String)requestParams.get((String)"left"));
+        left = Long.parseLong((String)requestParams.get((String)"left"));
 
         /*
          * Check for optional fields
@@ -141,7 +145,7 @@ public class TrackerRequestParser {
 
         // check for numwant
         if(requestParams.containsKey((String)"numwant")) {
-            Integer numWant = (Integer)requestParams.get((String)"numwant");
+            Integer numWant = Integer.parseInt((String)requestParams.get((String)"numwant"));
             // never give more than 50, less than 0 is not valid
             if(numWant < numPeersToReturn && numWant >= 0) {
                 // honour numwant from the client
@@ -159,7 +163,9 @@ public class TrackerRequestParser {
 
         // find torrent in database of tracked torrents
         try {
-            t = em.find(Torrent.class, infoHash);
+            Query q = em.createQuery("select t from Torrent t where t.infoHash = :infoHash");
+            q.setParameter("infoHash", infoHash);
+            t = (Torrent) q.getSingleResult();
 
             if(t == null) {
                 return(parseFailed("Torrent not tracked."));
@@ -179,48 +185,47 @@ public class TrackerRequestParser {
          * the first announce to the tracker from a new peer must have the
          * event = started key/value pair.
          */
-        if(requestParams.containsKey((String)"event")) {
-            // client just started the download
-            if(requestParams.get((String)"event") == "started") {
-                // enable this if needed
-                // is the peer already registered?
-                /*Query q = em.createNativeQuery("select p from Peer p " +
-                        "where p.ip = :ip or p.peerId = :peerId", Peer.class);
-                q.setParameter("ip", remoteAddress);
-                q.setParameter("peerId", peerId.getBytes());
 
+        if(requestParams.containsKey((String)"event")) {
+            event = (String) requestParams.get((String)"event");
+        }
+        if(!event.isEmpty()) {
+            // client just started the download
+            if(event.equalsIgnoreCase("started")) {
+                // is the peer already on this torrent?
+                Query q = em.createQuery("SELECT p FROM Peer p WHERE p.peerId = :peerId");
+                q.setParameter("peerId", peerId);
+
+                // there will only be zero or one results from this query. PeerID
+                // must be unique
                 List<Peer> res = q.getResultList();
                 if(!res.isEmpty()) {
-                    while(res.iterator().hasNext()) {
-                        Peer temp = res.iterator().next();
-
-                        // calculate amount of time inactive
-                        long inactivity = Calendar.getInstance().getTimeInMillis() -
-                                temp.getLastActionTime().getTime();
-
-                        // is the peer dead, or at least an approximation of dead?
-                        if(inactivity > ((defaultInterval + 120) * 1000)) {
-                            em.remove(temp);
-                        }
+                    // check for inactivity
+                    if(!peerIsInactive(res.get(0))) {
+                        // not inactive yet, and attempts at a new started
+                        Logger.getLogger(TrackerRequestParser.class.getName()).log(Level.WARNING,
+                            "event=started more than once. IP: " + remoteAddress.toString());
+                        em.getTransaction().rollback();
+                        em.close();
+                        return parseFailed("You are already on this torrent! Wait a while if this is not true.");
                     }
-                }*/
+                }
 
                 // add new peer
                 p = new Peer();
+
+                p.setPeerId(peerId);
 
                 p.setBytesLeft(left);
                 p.setDownloaded(downloaded);
                 p.setUploaded(uploaded);
 
-                Byte[] b = new Byte[2];
-                // lovely binary logic
-                b[0] = (byte)((port >> 8) & 0xFF);
-                b[1] = (byte)(port & 0xFF);
-
-                p.setPort(b);
+                p.setPort(port);
 
                 // set address
                 p.setIp(remoteAddress);
+
+                p.setLastActionTime(Calendar.getInstance().getTime());
 
                 // if left = 0, this peer has a complete copy, so add as seed,
                 // if not, add as leech
@@ -235,11 +240,43 @@ public class TrackerRequestParser {
                 em.persist(p);
             }
             // client stopped download
-            else if(requestParams.get((String)"event") == "stopped") {
+            else if(event.equalsIgnoreCase("stopped")) {
                 // remove peer from list
-                p = em.find(Peer.class, peerId.getBytes());
+                Query q = em.createQuery("SELECT p FROM Peer p WHERE p.peerId = :peerId");
+                q.setParameter("peerId", peerId);
+
+                // zero or one results returned
+                List<Peer> res = q.getResultList();
+                if(!res.isEmpty()) {
+                    p = res.get(0);
+                    em.remove(p);
+                }
+                else {
+                    Logger.getLogger(TrackerRequestParser.class.getName()).log(Level.WARNING,
+                            "event=stopped but no matching peer found. IP: " + remoteAddress.toString());
+                    em.getTransaction().rollback();
+                    em.close();
+                    return(parseFailed("Cannot stop a peer that has not started"));
+                }
+
+                // remove peer from torrent
+                if(p.isSeed()) {
+                    t.removeSeed(p);
+                }
+                else
+                    t.removeLeecher(p);
+                // no reason to give out more peers
+                numPeersToReturn = 0;
+                /*p = (Peer)q.getSingleResult();
                 if(p != null) {
                     em.remove(p);
+                }
+                else {
+                    Logger.getLogger(TrackerRequestParser.class.getName()).log(Level.WARNING,
+                            "event=stopped but no matching peer found");
+                    em.getTransaction().rollback();
+                    em.close();
+                    return(parseFailed("Cannot stop a peer that has not started"));
                 }
                 // remove peer from torrent
                 if(p.isSeed()) {
@@ -247,13 +284,17 @@ public class TrackerRequestParser {
                 }
                 else
                     p.getTorrent().removeLeecher(p);
+                // no reason to give out any more peers
+                numPeersToReturn = 0;*/
             }
             // client is now a seed
-            else if(requestParams.get((String)"event") == "completed") {
+            else if(event.equalsIgnoreCase("completed")) {
                 // peer is now seeding
+                // TODO: no more em.find
                 p = em.find(Peer.class, peerId.getBytes());
                 if(p != null) {
                     p.setSeed(true);
+                    returnSeeds = false;
                     p.getTorrent().removeLeecher(p);
                     p.getTorrent().addSeeder(p);
 
@@ -263,21 +304,42 @@ public class TrackerRequestParser {
             }
         } // if(event)
 
-        // update the data for the peer
-        p = em.find(Peer.class, peerId.getBytes());
-        // peer not found?
-        if(p == null) {
-            em.getTransaction().rollback();
-            em.close();
-            return(parseFailed("peer not in database and no event given?"));
+        // no event specified
+        else {
+            // update the data for the peer
+            Query q = em.createQuery("SELECT p FROM Peers p WHERE p.peerId = :peerId");
+            q.setParameter("peerId", peerId);
+            // TODO: no more getSingleResult()
+            p = (Peer) q.getSingleResult();
+            // peer not found?
+            if(p == null) {
+                em.getTransaction().rollback();
+                em.close();
+                return(parseFailed("peer not in database and no event given?"));
+            }
+
+            // update attributes
+            p.setBytesLeft(left);
+            p.setDownloaded(downloaded);
+            p.setUploaded(uploaded);
+            p.setLastActionTime(Calendar.getInstance().getTime());
+
+            returnSeeds = !p.isSeed();
+
+            em.merge(p);
+
         }
-
-        // update attributes
-        p.setBytesLeft(left);
-        p.setDownloaded(downloaded);
-        p.setUploaded(uploaded);
-        p.setLastActionTime(Calendar.getInstance().getTime());
-
+        // commit the changes made so far and close the EntityManager
+        try {
+            em.getTransaction().commit();
+        } catch(Exception ex) {
+            if(em.getTransaction().isActive())
+                em.getTransaction().rollback();
+            em.close();
+            throw ex;
+        }
+        em.close();
+        
         // populate the response
         responseParams.put((String)"complete", t.getNumSeeders().toString());
         responseParams.put((String)"incomplete", t.getNumLeechers().toString());
@@ -286,20 +348,18 @@ public class TrackerRequestParser {
 
         // find some peers!
         String peerList;
-        if(p.isSeed()) {
+        if(returnSeeds) {
             // only return leechers
-            peerList = getCompactPeerList((Peer[])t.getLeechersData().toArray(), numPeersToReturn);
+            Vector v = (Vector) t.getLeechersData();
+            peerList = getCompactPeerList(v, numPeersToReturn);
         }
         else {
             // return both seeds and leechers
-            peerList = getCompactPeerList((Peer[])t.getPeersData().toArray(), numPeersToReturn);
+            Vector v = (Vector) t.getPeersData();
+            peerList = getCompactPeerList(v, numPeersToReturn);
         }
 
         responseParams.put((String)"peers", peerList);
-
-        em.persist(p);
-        em.getTransaction().commit();
-        em.close();
 
         return(responseParams);
     }
@@ -348,16 +408,16 @@ public class TrackerRequestParser {
      * @return a peer list in the compact format ((4 bytes address + 2 bytes port) * amount)
      * as a String.
      */
-    private String getCompactPeerList(Peer[] population, Integer numWanted)
+    private String getCompactPeerList(Vector<Peer> population, Integer numWanted)
     {
         int numReturn = numWanted;
         String ret = new String();
         // is the population big enough to get all the peers we want?
-        if(population.length < numWanted) {
+        if(population.size() < numWanted) {
             // apparently not
             // return everything
-            for(int i = 0; i < population.length; i++) {
-                Peer p = population[i];
+            for(int i = 0; i < population.size(); i++) {
+                Peer p = population.get(i);
                 if(peerIsInactive(p)) {
                     continue;
                 }
@@ -377,10 +437,16 @@ public class TrackerRequestParser {
                 while(picked.get(next)) {
                     next = r.nextInt(numReturn);
                 }
-                ret += population[next].getCompactAddressPort();
+                ret += population.get(next).getCompactAddressPort();
                 picked.set(next);
             }
             // check chances for collisions?
+            // if numpeers is half the size or more of the swarm, drop the
+            // usual random pick-method, and simply pick either a sequential
+            // stream of peers either right-to-left or left-to-right?
+            // would greatly improve the performance of the algorithm on
+            // small swarms where peers to return is less than the total
+            // amount of peers
         }
         return ret;
     }
