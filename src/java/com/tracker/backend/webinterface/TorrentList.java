697e4398890fa07046cda81b026d2daad64677a9
bo <bo@75a8191c-d363-11dd-86c2-0f8696b7b6f9>
2008-11-24 18:03:19 +0000

    Documentation! :D:D:D:D
    git-svn-id: http://quash.googlecode.com/svn/trunk@48 75a8191c-d363-11dd-86c2-0f8696b7b6f9

--- src/java/com/tracker/backend/webinterface/TorrentList.java
+++ src/java/com/tracker/backend/webinterface/TorrentList.java
@@ -15,12 +15,24 @@ import java.util.Map;
  */
 public interface TorrentList {
     /**
-     * prints the torrentlist to the specified output with the specified options
+     * Prints the torrentlist to the specified output with the specified options
      * given in the request (search, include descriptions in search, etc). Used
      * for getting a standarised interface across different SoA-methods (XML and
      * JSON currently).
-     * @param requestMap the requests to parse and extract searchstrings from
-     * @param out the output writer to write the torrentlist to
+     * @param requestMap The requests to parse and extract searchstrings from.
+     * The keys that are significant in this are <em>"searchField"</em>, which may
+     * contain a string to search for, <em>"searchDescription"</em> which, if
+     * the value equals "checked", searches through torrent descriptions instead
+     * of just names, and finally <em>"includeDead"</em> which, if the value
+     * equals "checked", includes "dead" torrents in the resultset (ie, torrents
+     * which do not have any seeds or leechers.)
+     * @param out The output writer to write the torrentlist to. If used in jsp-
+     * files, the easiest way to get this i by doing a new PrintWriter(out),
+     * since "out" in that context is a JspWriter.
+     * @see XMLTorrentList
+     * @see JSONTorrentList
+     * @see JspWriter
+     * @see PrintWriter
      */
     public abstract void printTorrentList(Map<String, String[]> requestMap, PrintWriter out);
 }
