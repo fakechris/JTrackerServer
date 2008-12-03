0f464217c677fe432cba89a0b8057c8c0f6b1edc
bo <bo@75a8191c-d363-11dd-86c2-0f8696b7b6f9>
2008-12-03 18:19:57 +0000

    changed browse.jsp and .js to use the XML version of TorrentList.
    git-svn-id: http://quash.googlecode.com/svn/trunk@79 75a8191c-d363-11dd-86c2-0f8696b7b6f9

--- web/resources/behaviour/browse.js
+++ web/resources/behaviour/browse.js
@@ -4,22 +4,51 @@
  */
 
 // behaviour for browse.jsp
+
+/**
+ * Grabs a XML torrentlist from the server
+ */
 function getTorrentList(searchParameters) {
-    $.getJSON("json/torrentlist", searchParameters, function(json) {
-        // run on successful completion
-        $.each(json.name, function(i, name) {
-            $("#torrentName").append("#" + i + " " +name + "<br />");
-        });
-        $.each(json.date, function(i, date) {
-            $("#torrentDate").append("#" + i + " " +date + "<br />");
-        });
-        $("div:hidden").fadeIn("fast");
+    $.ajax({
+        type: 'GET',
+        dataType: 'xml',
+        error: displayError,
+        success: processBrowseResult
     });
 }
 
+/**
+ * processes the XML received from the server and populates the torrentlist.
+ */
+function processBrowseResult(reponseXML) {
+    $(responseXML).label('torrent').each(function(index) {
+        $("#torrentName").append("<a href=/Download?id=" + $(this).attr("id") +
+            ">" + "#" + i + " " + $(this).find("name").text() + "</a><br />");
+        $("#torrentDate").append($(this).find("dateAdded").text() + "<br />");
+    });
+    $("div:hidden").fadeIn("fast");
+}
+
+/**
+ * displays an error in case AJAX failed horribly.
+ */
+function displayError() {
+    $("#torrentName").append("An error occured while getting AJAX response.");
+    $("div:hidden").show();
+}
+
 $(document).ready(function() {
+    // bind the loading gif to the global ajax events
+    $("#loading").bind("ajaxSend", function() {
+        $(this).show();
+    }).bind("ajaxComplete", function() {
+       $(this).hide();
+    });
+
+    // fill the torrentlist
     getTorrentList({});
 
+    // bind the submit button to the getTorrentlist function
     $("#submit").click(function() {
         getTorrentList({
             searchField: $("#searchField").val(),
